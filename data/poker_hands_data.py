adcc3102b5ced8614cffbe4e2660eb64c994db4d
fardeenmohd <fardeenmohdpl@gmail.com>
2018-05-22 20:11:36 +0200

    + Add data classes for each type of data + Test parsing code in main.py

---
+++ data/poker_hands_data.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+
+DATA_PATH_TEST = "../resc/poker_hands/poker_hands_test.data"
+DATA_PATH_TRAIN = "../resc/poker_hands/poker_hands_train.data"
+
+
+class PokerHandsData:
+
+    def __init__(self, number_of_inputs: int = 2000, train_percentage: int = 0.8):
+        self.train_data = pd.read_csv(DATA_PATH_TEST)
+        self.test_data = pd.read_csv(DATA_PATH_TRAIN)
+        self.train_values = self.train_data.values
+        self.test_values = self.test_data.values
+        self.total_data_rows = self.train_data.values.shape[0] + self.test_data.values.shape[0]
+        self.number_of_rows = number_of_inputs if number_of_inputs <= self.total_data_rows else self.total_data_rows
+        self.number_of_columns = self.test_values.shape[1]
+        self.train_percentage = train_percentage
+        self.test_percentage = round(1 - train_percentage, 2)
+        self.train_count = int(self.train_percentage * self.number_of_rows)
+        self.test_count = int(self.test_percentage * self.number_of_rows)
+        self.train_values = self.train_values[:self.train_count, :]
+        self.test_values = self.test_values[:self.test_count, :]
+        print("-----Parsed Poker Hands Data-----")
+        print("Total input rows demanded: " + number_of_inputs.__str__() + " Total input rows parsed: " +
+              (self.train_count + self.test_count).__str__())
+        print("Test count: " + (self.test_values.shape[0]).__str__() + " Train count: " +
+              (self.train_values.shape[0]).__str__())
+        print("Number of Attributes: " + self.number_of_columns.__str__() )
---
+++ data/wine_quality_data.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+from enum import Enum
+
+DATA_PATH_WHITE = "../resc/wine_quality/wine_quality_white.csv"
+DATA_PATH_RED = "../resc/wine_quality/wine_quality_red.csv"
+
+
+class WineType(Enum):
+    WHITE = 1
+    RED = 2
+
+
+class WineQualityData:
+
+    def __init__(self, number_of_inputs: int = 2000, train_percentage: int = 0.8, wine_type: WineType = WineType.WHITE):
+        self.wine_type = wine_type
+        self.data = pd.read_csv(DATA_PATH_RED, sep=';') if wine_type == WineType.RED else pd.read_csv(DATA_PATH_WHITE,
+                                                                                                      sep=';')
+        self.values = self.data.values
+        self.number_of_rows = self.values.shape[0]
+        self.number_of_columns = self.values.shape[1]
+        self.number_of_inputs = number_of_inputs if number_of_inputs <= self.number_of_rows else self.number_of_rows
+        self.number_of_rows = self.number_of_inputs
+        self.train_percentage = train_percentage
+        self.test_percentage = round(1 - train_percentage, 2)
+        self.train_count = int(self.train_percentage * self.number_of_rows)
+        self.test_count = int(self.test_percentage * self.number_of_rows)
+        self.train_values = self.values[:self.train_count, :]
+        self.test_values = self.values[:self.test_count, :]
+        print("-----Parsed " + self.wine_type.name + " Wine Quality Data-----")
+        print("Total input rows demanded: " + number_of_inputs.__str__() + " Total input rows parsed: " +
+              (self.train_count + self.test_count).__str__())
+        print("Test count: " + (self.test_values.shape[0]).__str__() + " Train count: " +
+              (self.train_values.shape[0]).__str__())
+        print("Number of Attributes: " + self.number_of_columns.__str__())
